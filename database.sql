5579f92f1f22799126af4f076ebb11a689773f06
travismack47 <travismack47@protonmail.com>
2023-06-19 13:32:52 -0500

    added comments to database file, got all trader page get requests set up

--- database.sql
+++ database.sql
@@ -3,6 +3,9 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+---------- Create tables for the database -----------
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
@@ -38,6 +41,8 @@ CREATE TABLE notes (
   "timestamp" TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES "user"(id)
 );
+
+---------- Insert statements to add data into the tables -------------
 
 INSERT INTO traders (name)
 VALUES ('Prapor'),
--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -5,10 +5,105 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
+
+// GET routes for each Trader page and Notes page 
+router.get('/prapor', (req, res) => {
+  const query = `SELECT quests.id, quests.name, quests.description
+  FROM quests
+  JOIN traders ON traders.id = quests.trader_id
+  WHERE traders.id = 1;`
+  pool.query(query)
+  .then((response) => {
+    console.log(response);
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log('error getting quests', error);
+  })
 });
 
+router.get('/therapist', (req, res) => {
+  const query = `SELECT quests.id, quests.name, quests.description
+  FROM quests
+  JOIN traders ON traders.id = quests.trader_id
+  WHERE traders.id = 2;`
+  pool.query(query)
+  .then((response) => {
+    console.log(response);
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log('error getting quests', error);
+  })
+});
+
+router.get('/skier', (req, res) => {
+  const query = `SELECT quests.id, quests.name, quests.description
+  FROM quests
+  JOIN traders ON traders.id = quests.trader_id
+  WHERE traders.id = 3;`
+  pool.query(query)
+  .then((response) => {
+    console.log(response);
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log('error getting quests', error);
+  })
+});
+
+router.get('/jaeger', (req, res) => {
+  const query = `SELECT quests.id, quests.name, quests.description
+  FROM quests
+  JOIN traders ON traders.id = quests.trader_id
+  WHERE traders.id = 4;`
+  pool.query(query)
+  .then((response) => {
+    console.log(response);
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log('error getting quests', error);
+  })
+});
+
+router.get('/peacekeeper', (req, res) => {
+  const query = `SELECT quests.id, quests.name, quests.description
+  FROM quests
+  JOIN traders ON traders.id = quests.trader_id
+  WHERE traders.id = 5;`
+  pool.query(query)
+  .then((response) => {
+    console.log(response);
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log('error getting quests', error);
+  })
+});
+
+router.get('/mechanic', (req, res) => {
+  const query = `SELECT quests.id, quests.name, quests.description
+  FROM quests
+  JOIN traders ON traders.id = quests.trader_id
+  WHERE traders.id = 6;`
+  pool.query(query)
+  .then((response) => {
+    console.log(response);
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log('error getting quests', error);
+  })
+});
+
+router.get('/ragman', (req, res) => {
+  const query = `SELECT quests.id, quests.name, quests.description
+  FROM quests
+  JOIN traders ON traders.id = quests.trader_id
+  WHERE traders.id = 7;`
+  pool.query(query)
+  .then((response) => {
+    console.log(response);
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log('error getting quests', error);
+  })
+});
 /**
  * POST route template
  */
