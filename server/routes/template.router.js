2bf8ed6c8ba9acb9fb10ceeb1918054b41873322
travismack47 <travismack47@protonmail.com>
2023-06-19 14:12:24 -0500

    added GET and POST requests for the Notes page

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -2,113 +2,75 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+// GET request for each Trader page //
 
-// GET routes for each Trader page and Notes page 
-router.get('/prapor', (req, res) => {
-  const query = `SELECT quests.id, quests.name, quests.description
+router.get('/trader/:trader_id', (req, res) => {
+  const traderId = req.params.trader_id
+
+  const query = `
+  SELECT quests.id, quests.name, quests.description
   FROM quests
   JOIN traders ON traders.id = quests.trader_id
-  WHERE traders.id = 1;`
+  WHERE traders.id = ${traderId};
+  `
   pool.query(query)
-  .then((response) => {
-    console.log(response);
-    res.send(result.rows)
-  }).catch((error) => {
-    console.log('error getting quests', error);
-  })
+    .then((response) => {
+      console.log(response);
+      res.send(result.rows)
+    }).catch((error) => {
+      console.log('error getting Prapor`s quests', error);
+      res.sendStatus(500);
+    })
 });
 
-router.get('/therapist', (req, res) => {
-  const query = `SELECT quests.id, quests.name, quests.description
-  FROM quests
-  JOIN traders ON traders.id = quests.trader_id
-  WHERE traders.id = 2;`
-  pool.query(query)
-  .then((response) => {
-    console.log(response);
-    res.send(result.rows)
-  }).catch((error) => {
-    console.log('error getting quests', error);
-  })
+// End Trader page GET request //
+
+// GET request for Notes page //
+
+router.get('/notes', (req, res) => {
+  const userId = req.user.id; 
+  const query = `
+    SELECT "user_id", "title", "description", "timestamp"
+    FROM notes
+    WHERE "user_id" = $1;
+  `;
+  const values = [userId]; // Pass the user ID as a parameter //
+  pool.query(query, values)
+    .then((result) => {
+      console.log(result);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('Error getting notes:', error);
+      res.sendStatus(500);
+    });
 });
 
-router.get('/skier', (req, res) => {
-  const query = `SELECT quests.id, quests.name, quests.description
-  FROM quests
-  JOIN traders ON traders.id = quests.trader_id
-  WHERE traders.id = 3;`
-  pool.query(query)
-  .then((response) => {
-    console.log(response);
-    res.send(result.rows)
-  }).catch((error) => {
-    console.log('error getting quests', error);
-  })
+//End GET request for Notes page //
+
+// POST request for adding new notes from Notes page //
+
+router.post('/notes', (req, res) => {
+  const userId = req.user.id; 
+  const { title, description } = req.body;
+
+  const query = `
+    INSERT INTO notes ("user_id", "title", "description", "timestamp")
+    VALUES ($1, $2, $3, NOW())
+    RETURNING *;
+  `;
+  const values = [userId, title, description];
+
+  pool.query(query, values)
+    .then((result) => {
+      console.log(result.rows[0]);
+      res.send(result.rows[0]);
+    })
+    .catch((error) => {
+      console.log('Error adding new note:', error);
+      res.status(500).send('An error occurred while adding a new note.');
+    });
 });
 
-router.get('/jaeger', (req, res) => {
-  const query = `SELECT quests.id, quests.name, quests.description
-  FROM quests
-  JOIN traders ON traders.id = quests.trader_id
-  WHERE traders.id = 4;`
-  pool.query(query)
-  .then((response) => {
-    console.log(response);
-    res.send(result.rows)
-  }).catch((error) => {
-    console.log('error getting quests', error);
-  })
-});
-
-router.get('/peacekeeper', (req, res) => {
-  const query = `SELECT quests.id, quests.name, quests.description
-  FROM quests
-  JOIN traders ON traders.id = quests.trader_id
-  WHERE traders.id = 5;`
-  pool.query(query)
-  .then((response) => {
-    console.log(response);
-    res.send(result.rows)
-  }).catch((error) => {
-    console.log('error getting quests', error);
-  })
-});
-
-router.get('/mechanic', (req, res) => {
-  const query = `SELECT quests.id, quests.name, quests.description
-  FROM quests
-  JOIN traders ON traders.id = quests.trader_id
-  WHERE traders.id = 6;`
-  pool.query(query)
-  .then((response) => {
-    console.log(response);
-    res.send(result.rows)
-  }).catch((error) => {
-    console.log('error getting quests', error);
-  })
-});
-
-router.get('/ragman', (req, res) => {
-  const query = `SELECT quests.id, quests.name, quests.description
-  FROM quests
-  JOIN traders ON traders.id = quests.trader_id
-  WHERE traders.id = 7;`
-  pool.query(query)
-  .then((response) => {
-    console.log(response);
-    res.send(result.rows)
-  }).catch((error) => {
-    console.log('error getting quests', error);
-  })
-});
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
-});
 
 module.exports = router;
