745da3b8840bce41a7c6067453e96fab5c85eb15
travismack47 <travismack47@protonmail.com>
2023-06-26 17:03:51 -0500

    removed unnecessary code from notes reducer file

--- src/redux/reducers/notes.reducer.js
+++ src/redux/reducers/notes.reducer.js
@@ -5,15 +5,6 @@ const notesReducer = (state = initialState, action) => {
   switch (action.type) { // Switch statement handling the different action types // 
     case 'SET_NOTES':
       return action.payload;
-
-    case 'ADD_NOTE': // Adds a new note to the database //
-      return [...state, action.payload];
-
-    case 'UPDATE_NOTE':
-      return state.map((note) => // Maps over the notes and checks each note item to see if it matches the action.payload. If it does,
-      // that means it is the correct note to edit. If it doesn't, it just returns note as is //
-        note.id === action.payload.id ? action.payload : note
-      );
     default:
       return state;
   }
