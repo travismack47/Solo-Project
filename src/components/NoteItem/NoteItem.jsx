d09ff63ae0f5cca437da501c7ba744e1aba96b14
travismack47 <travismack47@protonmail.com>
2023-06-29 10:01:14 -0500

    deleted unnecessary images from project, added sweet alert for when user saves edited note, messed around with form and table styling on Notes page

--- src/components/NoteItem/NoteItem.jsx
+++ src/components/NoteItem/NoteItem.jsx
@@ -1,43 +1,52 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { TableRow, TableCell, TextField, Button, Modal, Box } from "@mui/material"; // Material-UI imports //
-import moment from "moment"; // Moment.js import for displaying formatted timestamps
+import { TableRow, TableCell, TextField, Button, Modal, Box } from "@mui/material";
+import moment from "moment";
 import Swal from "sweetalert2";
 
 const NoteItem = ({ note }) => {
   const dispatch = useDispatch();
   const [newNoteTitle, setNewNoteTitle] = useState(note.title);
   const [newNoteDesc, setNewNoteDesc] = useState(note.description);
-  const [isEditable, setIsEditable] = useState(false); // Default edit state is set to false //
-  const [isModalOpen, setIsModalOpen] = useState(false); // Default modal open display is set to false //
+  const [isEditable, setIsEditable] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleEdit = () => {
-    setIsModalOpen(true); // Handles opening the modal when the Edit button is clicked //
+    setIsModalOpen(true);
   };
 
   const handleCancel = () => {
-    setIsModalOpen(false); // Handles closing the modal when the Cancel button is clicked //
+    setIsModalOpen(false);
   };
 
   const handleUpdate = () => {
+    Swal.fire({
+      title: 'Note has been updated',
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    });
     dispatch({
       type: "UPDATE_NOTE",
-      payload: { // Sending the new title, new description, and note ID as the payload //
+      payload: {
         title: newNoteTitle,
         description: newNoteDesc,
         noteId: note.id,
       },
     });
+
     setIsEditable(false);
-    setIsModalOpen(false); // Dispatches the update action and closes the modal/ changes edit state when the Save button is clicked //
+    setIsModalOpen(false);
   };
 
   const handleDelete = () => {
-    dispatch({ type: "DELETE_NOTE", payload: { noteId: note.id } }); // Dispatches the delete action when the Delete button is clicked
+    dispatch({ type: "DELETE_NOTE", payload: { noteId: note.id } });
   };
 
-  const swal = () => { // Sweet alert function that pops up when the delete button is clicked asking user to confirm if they want
-    // to delete that note. If they click yes, a success alert pops up and the note is deleted //
+  const swal = () => {
     Swal.fire({
       title: 'Are you sure?',
       text: "You won't be able to revert this!",
@@ -52,7 +61,7 @@ const NoteItem = ({ note }) => {
           'Deleted!',
           'Your note has been deleted.',
           'success',
-          handleDelete() // Handles deleting the note if user confirms the delete on the alert //
+          handleDelete()
         );
       };
     });
@@ -65,11 +74,11 @@ const NoteItem = ({ note }) => {
           {isEditable ? (
             <TextField
               type="text"
-              value={newNoteTitle} // Displays existing note's title text //
-              onChange={(e) => setNewNoteTitle(e.target.value)} // Handles the input text for the updated title //
+              value={newNoteTitle}
+              onChange={(e) => setNewNoteTitle(e.target.value)}
             />
           ) : (
-            note.title // Displays the note's title when not in edit mode //
+            note.title
           )}
         </TableCell>
         <TableCell>
@@ -77,73 +86,63 @@ const NoteItem = ({ note }) => {
             <TextField
               multiline
               rows={3}
-              value={newNoteDesc} // Displays existing notes description text //
-              onChange={(e) => setNewNoteDesc(e.target.value)} // Handles the input text for the updated description //
+              value={newNoteDesc}
+              onChange={(e) => setNewNoteDesc(e.target.value)}
             />
           ) : (
-            note.description // Displays the note's description when not in edit mode //
+            note.description
           )}
         </TableCell>
-        <TableCell>{moment(note.timestamp).calendar()}</TableCell> {/* Displays the formatted timestamp using Moment.js */}
+        <TableCell>{moment(note.timestamp).calendar()}</TableCell>
         <TableCell>
-          {isEditable ? ( // Displays these buttons when the note is in edit mode //
+          {isEditable ? (
             <>
-              {/* Cancel button */}
-              <Button variant="contained" color="secondary" onClick={handleCancel}> {/* Handles canceling a note edit */}
+              <Button variant="contained" onClick={handleCancel}>
                 Cancel
               </Button>
-              {/* Save button */}
-              <Button variant="contained" color="primary" onClick={handleUpdate}> {/* Handles submitting an updated note */}
+              <Button variant="contained" color="primary" onClick={handleUpdate}>
                 Save
               </Button>
             </>
-          ) : ( // Displays these buttons when the note is not in edit mode //
+          ) : (
             <>
-              {/* Edit button */}
-              <Button variant="contained" color="primary" onClick={handleEdit}> {/* Handles switching to edit mode on a note */}
+              <Button variant="contained" color="primary" onClick={handleEdit}>
                 Edit
-              </Button>{" "}
-              {/* Delete button */}
-              <Button variant="contained" color="error" onClick={swal}> {/* Handles deleting an existing note */}
+              </Button>
+              <Button variant="contained" color="error" onClick={swal}>
                 Delete
               </Button>
             </>
           )}
         </TableCell>
       </TableRow>
-      {/* Modal for editing note */}
       <Modal
         open={isModalOpen}
         onClose={handleCancel}
-        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }} // Styles the modal to be centered both horizontally 
-      // and vertically on the page //
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
       >
-        <Box sx={{ bgcolor: "background.paper", p: 4 }}> {/* Sets the background color of the modal */}
-          {/* Title input field */}
+        <Box sx={{ bgcolor: "background.paper", p: 4 }}>
           <TextField
             type="text"
             label="Title"
             value={newNoteTitle}
-            onChange={(e) => setNewNoteTitle(e.target.value)} // Handles the text of the newly edited note title //
+            onChange={(e) => setNewNoteTitle(e.target.value)}
             fullWidth
-            sx={{ mb: 2 }} // Adds margin bottom to the input field // 
+            sx={{ mb: 2 }}
           />
-          {/* Description input field */}
           <TextField
             multiline
             rows={3}
             label="Description"
             value={newNoteDesc}
-            onChange={(e) => setNewNoteDesc(e.target.value)} // Handles the text of the newly edited note description //
+            onChange={(e) => setNewNoteDesc(e.target.value)}
             fullWidth
-            sx={{ mb: 2 }} // Adds margin bottom to the input field //
+            sx={{ mb: 2 }}
           />
-          {/* Cancel button */}
-          <Button variant="contained" color="secondary" onClick={handleCancel} sx={{ mr: 2 }}> {/* Styles the cancel button */}
+          <Button variant="contained" color="secondary" onClick={handleCancel} sx={{ mr: 2 }}>
             Cancel
           </Button>
-          {/* Save button */}
-          <Button variant="contained" color="primary" onClick={handleUpdate}> {/* Styles the save button */}
+          <Button variant="contained" color="primary" onClick={handleUpdate}>
             Save
           </Button>
         </Box>
--- src/components/Pages/Notes/Notes.jsx
+++ src/components/Pages/Notes/Notes.jsx
@@ -10,105 +10,110 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Box,
+  Paper,
 } from "@mui/material";
 import NoteItem from "../../NoteItem/NoteItem";
-import { Pagination } from "@mui/material";
-import './Notes.css'
+import "./Notes.css";
 
 const Notes = () => {
   const dispatch = useDispatch();
   const notes = useSelector((store) => store.notes);
   const [newNoteTitle, setNewNoteTitle] = useState("");
   const [newNoteDesc, setNewNoteDesc] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const notesPerPage = 5;
 
   useEffect(() => {
-    dispatch({ type: "FETCH_NOTES" }); // Fetches the notes from the server when the component mounts //
+    dispatch({ type: "FETCH_NOTES" }); // Fetches the notes from the server when the component mounts
   }, []);
 
   const handleNote = () => {
     dispatch({
       type: "ADD_NOTE",
       payload: { title: newNoteTitle, description: newNoteDesc },
-    }); // Dispatches ADD_NOTE action with title and description as the payload //
-    setNewNoteTitle(""); // Resets input fields when a new note is added and action is dispatched //
+    });
+    setNewNoteTitle("");
     setNewNoteDesc("");
-  };
-
-  const indexOfLastNote = currentPage * notesPerPage;
-  const indexOfFirstNote = indexOfLastNote - notesPerPage;
-  const currentNotes = notes.slice(indexOfFirstNote, indexOfLastNote);
-
-  const handleChangePage = (event, page) => {
-    setCurrentPage(page);
   };
 
   return (
     <>
       <div className="notes-page">
         <div className="background-imagenotes" />
-      <Container className="mt-4">
-        <form>
-          {/* Input field for note title */}
-          <TextField
-            label="Title"
-            variant="filled"
-            value={newNoteTitle}
-            onChange={(e) => setNewNoteTitle(e.target.value)} // Handles the input text for the new note's title //
-            placeholder="Enter note title"
-            fullWidth
-            margin="normal"
-          />
-          {/* Input field for note description */}
-          <TextField
-            label="Description"
-            variant="filled"
-            value={newNoteDesc}
-            onChange={(e) => setNewNoteDesc(e.target.value)} // Handles the input text for the new note's description //
-            placeholder="Enter note description"
-            multiline
-            rows={5}
-            fullWidth
-            margin="normal"
-          />
-          {/* Button for saving the new note */}
-          <Button variant="contained" color="primary" onClick={handleNote}>
-            Save Note
-          </Button>
-        </form>
-        
-        {/* Pagination */}
-        <Pagination
-          count={Math.ceil(notes.length / notesPerPage)}
-          page={currentPage}
-          onChange={handleChangePage}
-          color="primary"
-          style={{ marginBottom: "1rem" }}
-        />
+        <div style={{ marginTop: "2rem" }} />
+        <Container className="mt-4">
+          {/* Form container */}
+          <Paper elevation={3} className="form-container">
+            <form>
+              {/* Input field for note title */}
+              <TextField
+                label="Title"
+                variant="filled"
+                value={newNoteTitle}
+                onChange={(e) => setNewNoteTitle(e.target.value)} // Handles the input text for the new note's title
+                placeholder="Enter note title"
+                fullWidth
+                margin="normal"
+              />
+              {/* Input field for note description */}
+              <TextField
+                label="Description"
+                variant="filled"
+                value={newNoteDesc}
+                onChange={(e) => setNewNoteDesc(e.target.value)} // Handles the input text for the new note's description
+                placeholder="Enter note description"
+                multiline
+                rows={5}
+                fullWidth
+                margin="normal"
+              />
+              {/* Button for saving the new note */}
+              <Button variant="contained" color="primary" onClick={handleNote}>
+                Save Note
+              </Button>
+            </form>
+          </Paper>
 
-        {/* Table for displaying the list of notes */}
-        <TableContainer component={Box}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {/* Table columns */}
-                <TableCell>Title</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell>Timestamp</TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {/* Mapping over the currentNotes array and rendering NoteItem for each note */}
-              {currentNotes.map((note) => (
-                <NoteItem key={note.id} note={note} /> // Passing note as a prop to NoteItem component //
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
+          <div style={{ marginTop: "2rem" }} />
+
+          {/* Container for the table */}
+          <div className="table-container">
+            {/* Table for displaying the list of notes */}
+            <TableContainer
+              component={Paper}
+              elevation={3}
+              sx={{
+                width: "100%",
+                margin: "0 auto",
+                backgroundColor: "rgba(255, 255, 255, 0.75)",
+              }}
+            >
+              <Table sx={{ minWidth: 700 }}>
+                <TableHead>
+                  <TableRow>
+                    {/* Table columns */}
+                    <TableCell sx={{ maxWidth: 300 }}>Title</TableCell>
+                    <TableCell
+                    sx={{
+                      maxWidth: 300,
+                      whiteSpace: "pre-wrap",
+                      wordWrap: "break-word",
+                    }}
+                  >
+                      Description
+                    </TableCell>
+                    <TableCell sx={{ width: 150 }}>Timestamp</TableCell>
+                    <TableCell sx={{ width: 150 }} align="right"></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {/* Mapping over the notes array and rendering NoteItem for each note */}
+                  {notes.map((note) => (
+                    <NoteItem key={note.id} note={note} /> // Passing note as a prop to NoteItem component
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </Container>
       </div>
     </>
   );
