bbd0c6dc96fcf2a9654c4c3ad150b6dfc8455bac
travismack47 <travismack47@protonmail.com>
2023-08-17 00:15:37 -0500

    changed style of log out button on user welcome page

--- src/components/Pages/UserPage/UserPage.jsx
+++ src/components/Pages/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, styled, ButtonBase } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import './UserPage.css';
 import Swal from 'sweetalert2';
@@ -10,6 +10,31 @@ function UserPage() {
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const StyledButton = styled(ButtonBase)(({ theme }) => ({
+    padding: '10px 20px',
+    margin: '10px',
+    borderRadius: '10px',
+    fontWeight: 'bold',
+    fontSize: '13px',
+    transition: 'all 0.3s ease',
+    border: '2px solid transparent',
+    '&:hover': {
+      transform: 'scale(1.05)',
+      boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.1)',
+      borderColor: 'transparent',
+    },
+  }));
+
+  const LogOutButton = styled(StyledButton)({
+    color: '#e63946',  // Deep coral red
+    borderColor: '#e63946',
+    backgroundColor: 'transparent',
+    '&:hover': {
+      backgroundColor: '#e63946',
+      color: '#fff',
+    },
+  });
   
   const handleLogout = () => {
     Swal.fire({
@@ -40,9 +65,9 @@ function UserPage() {
         <Typography variant="h2" gutterBottom color={'white'}>
           Welcome, {user.username}!
         </Typography>
-        <Button variant="contained" color="primary" onClick={handleLogout} type='logout' id='logout-btn'>
-          Log Out
-        </Button>
+      <LogOutButton onClick={handleLogout}>
+        Log Out
+      </LogOutButton>
       </div>
     </div>
   );
